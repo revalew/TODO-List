81ebc09a3b7ce58788d5abbece5ef3374a14721b
revalew <115182776+revalew@users.noreply.github.com>
2023-02-27 14:15:46 +0100

    used queries to acquire elements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-// Strict mode aplies some restrictions so we could not make stupid things (which are obviously allowed in JS by defaulf), e.g., creating variables without specifying their type, i.e., const / let
+// Strict mode aplies some restrictions so we could not make stupid things (which are obviously allowed in JS by default), e.g., creating variables without specifying their type, i.e., const / let
 'use strict'
 // -------------------------------------
+let todoInput // place to input the task
+let errorInfo // info about no tasks / no text in input
+let addBtn // ADD button - adding new elements to list
+let ulList // task list, li tags
+
+const main = () => {
+    // calling functions
+    prepareDOMElements()
+    prepareDOMEvents()
+}
+const prepareDOMElements = () => {
+    // acquiring all elements
+    todoInput = document.querySelector('.todo-input')
+    errorInfo = document.querySelector('.error-info')
+    addBtn = document.querySelector('.btn-add')
+    ulList = document.querySelector('.todolist ul')
+}
+const prepareDOMEvents = () => {
+    // listener
+}
+
+document.addEventListener('DOMContentLoaded', main)
