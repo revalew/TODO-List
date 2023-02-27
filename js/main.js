018f812b07d7ba60cafec9b54215d3d2ea419d91
revalew <115182776+revalew@users.noreply.github.com>
2023-02-27 15:03:41 +0100

    created function to check which tool button is pressed

--- js/main.js
+++ js/main.js
@@ -22,6 +22,8 @@ const prepareDOMElements = () => {
 const prepareDOMEvents = () => {
 	// listeners
 	addBtn.addEventListener('click', addNewTask)
+    ulList.addEventListener('click', checkClick)
+
 }
 
 const addNewTask = () => {
@@ -54,7 +56,22 @@ const createToolsArea = () => {
     deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
 
 	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+    // toolsPanel.addEventListener('click', checkClick)
+
 	newTodo.append(toolsPanel)
+}
+
+const checkClick = (e) => {
+    if (e.target.matches('.complete')) {
+        e.target.closest('li').classList.toggle('completed')
+        e.target.classList.toggle('completed')
+    } else if (e.target.matches('.edit')) {
+        
+    } else if (e.target.matches('.delete')) {
+
+    } else {
+
+    }
 }
 
 document.addEventListener('DOMContentLoaded', main)
