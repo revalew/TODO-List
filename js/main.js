38445f1461b38bd3fe7c0d57b1d7b185390f3813
revalew <115182776+revalew@users.noreply.github.com>
2023-02-27 14:27:44 +0100

    created function to add new tasks

--- js/main.js
+++ js/main.js
@@ -5,21 +5,35 @@ let todoInput // place to input the task
 let errorInfo // info about no tasks / no text in input
 let addBtn // ADD button - adding new elements to list
 let ulList // task list, li tags
+let newTodo // new li element / task / todo
 
 const main = () => {
-    // calling functions
-    prepareDOMElements()
-    prepareDOMEvents()
+	// calling functions
+	prepareDOMElements()
+	prepareDOMEvents()
 }
 const prepareDOMElements = () => {
-    // acquiring all elements
-    todoInput = document.querySelector('.todo-input')
-    errorInfo = document.querySelector('.error-info')
-    addBtn = document.querySelector('.btn-add')
-    ulList = document.querySelector('.todolist ul')
+	// acquiring all elements
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
 }
 const prepareDOMEvents = () => {
-    // listener
+	// listeners
+	addBtn.addEventListener('click', addNewTask)
+}
+
+const addNewTask = () => {
+	if (todoInput.value !== '') {
+		newTodo = document.createElement('li')
+		newTodo.textContent = todoInput.value
+		ulList.append(newTodo)
+		todoInput.value = ''
+		errorInfo.textContent = ''
+	} else {
+		errorInfo.textContent = 'Wpisz treść zadania!'
+	}
 }
 
 document.addEventListener('DOMContentLoaded', main)
