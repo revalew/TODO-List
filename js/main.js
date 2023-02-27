131c6b75512b52c145b7ce7bbbfe30ee60834e90
revalew <115182776+revalew@users.noreply.github.com>
2023-02-27 14:48:45 +0100

    created function to add edit tools

--- js/main.js
+++ js/main.js
@@ -28,12 +28,33 @@ const addNewTask = () => {
 	if (todoInput.value !== '') {
 		newTodo = document.createElement('li')
 		newTodo.textContent = todoInput.value
+        createToolsArea()
 		ulList.append(newTodo)
 		todoInput.value = ''
 		errorInfo.textContent = ''
 	} else {
 		errorInfo.textContent = 'Wpisz treść zadania!'
 	}
+}
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+
+	const completeBtn = document.createElement('button')
+	completeBtn.classList.add('complete')
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+    
+	const editBtn = document.createElement('button')
+	editBtn.classList.add('edit')
+    editBtn.textContent = 'EDIT'
+    
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
+	newTodo.append(toolsPanel)
 }
 
 document.addEventListener('DOMContentLoaded', main)
